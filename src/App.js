a1561dd48dcf90e19ac8de33c2a05ab0b27ab5d2
Prajit-Sengupta <ratulsengupta8@gmail.com>
2021-08-25 19:42:28 +0530

    Changes made for deployment|| Commented Router Functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react'
 import Alert from './components/Alert';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-  } from "react-router-dom";
+// import {
+//     BrowserRouter as Router,
+//     Switch,
+//     Route,
+//   } from "react-router-dom";
 
 function App() {
 
@@ -46,23 +46,23 @@ function App() {
   }
   return (
     <> 
-    <Router>
+    {/* <Router> */}
     <Navbar title="TextUtils" mode={Mode} toggleMode={toggleMode} />
     <Alert alert={alert}/>
     <div className="container my-3">
-    <Switch>
+    {/* <Switch>
           <Route exact path="/about">
-            <About/>
+            <About mode={Mode}/>
           </Route>
           
-          <Route exact path="/">
+          <Route exact path="/"> */}
             <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={Mode}/>
-          </Route>
-        </Switch>
+          {/* </Route>
+        </Switch> */}
         
         
     </div>
-    </Router>
+    {/* </Router> */}
     </>
   );
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -42,19 +42,19 @@ export default function TextForm(props) {
     return (
         <>
         <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
-            <h1>{props.heading}</h1>
+            <h1 className="mb-3">{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" id="myBox" value={text} style={{backgroundColor: props.mode==='light'?'white':'grey', color: props.mode==='dark'?'white':'black'}} onChange={handleOnChange} rows="8"></textarea>
+            <textarea className="form-control" id="myBox" value={text} style={{backgroundColor: props.mode==='light'?'white':'#13466e', color: props.mode==='dark'?'white':'black'}} onChange={handleOnChange} rows="8"></textarea>
             </div> 
-            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to lowercase</button>
-            <button className="btn btn-primary mx-1" onClick={handleAlClick}>AlTeRnAtInG CaSe</button>
-            <button className="btn btn-primary mx-1" onClick={handleClClick}>Clear</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to uppercase</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to lowercase</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleAlClick}>AlTeRnAtInG CaSe</button>
+            <button className="btn btn-primary mx-1 my-1" onClick={handleClClick}>Clear</button>
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}} >
             <h2>Your Text Summary</h2>
-            <p>{text.split(" ").length} words, {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words, {text.length} characters</p>
+            <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <h2>Text Preview</h2>
             <p>{text.length>0? text: 'Enter something in the text to preview it here'}</p>
         </div>
@@ -67,7 +67,8 @@ export default function TextForm(props) {
 
 
 
-
+//text.split(" ")--> split the text where there is a gap and return an array
+//.filter()--> takes a function and if it comes true than returns it values to the array.
 
 
 
